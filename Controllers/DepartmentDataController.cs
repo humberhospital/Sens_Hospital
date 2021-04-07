9e67f3575c71d4cf4142cebed8da572e1ea2aa9f
moeasakr <moeasakr@gmail.com>
2021-04-07 19:42:18 -0400

    Created department controller

---
+++ Controllers/DepartmentDataController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sens_Hospital.Models;
+
+namespace Sens_Hospital.Controllers
+{
+    public class DepartmentDataController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: DepartmentData
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetDepartments()
+        {
+            List<Department> Departments = db.Departments.ToList();
+            List<DepartmentDto> DepartmentDtos = new List<DepartmentDto>();
+
+            foreach (var department in Departments)
+            {
+                DepartmentDto NewDepartment = new DepartmentDto
+                {
+                    DepartmentID = department.DepartmentID,
+                    DepartmentName = department.DepartmentName,
+                    DepartmentPhoneNumber = department.DepartmentPhoneNumber
+                };
+                DepartmentDtos.Add(NewDepartment);
+            }
+
+            return View(DepartmentDtos);
+        }
+    }
+}

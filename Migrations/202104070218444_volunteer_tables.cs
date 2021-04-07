badb0cb1c9d48fbdf87ab210e46b30627383c22a
elle robertson <elea.roberts@hotmail.com>
2021-04-06 22:22:44 -0400

    volunteer models

---
+++ Migrations/202104070218444_volunteer_tables.cs
@@ -0,0 +1,16 @@
+namespace Sens_Hospital.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class volunteer_tables : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

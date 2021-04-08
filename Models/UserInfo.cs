090c5bd8bad7796f39fc754110b7d5bf05a4ebb4
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-08 16:05:23 -0400

    Updated cs files

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sens_Hospital.Models
 {
@@ -16,5 +18,24 @@ namespace Sens_Hospital.Models
         public string Address { get; set; }
         public string PostalCode { get; set; }
         public string PhoneNumber { get; set; }
+    }
+
+
+    public class UserInfoDto
+    {
+        public int UserID { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Street number")]
+        public int StreetNumber { get; set; }
+        [DisplayName("Address")]
+        public string Address { get; set; }
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
     }
 }
